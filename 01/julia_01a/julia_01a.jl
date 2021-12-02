80c5f2b04d1113620e9c0363ec771fac98a10f16
Gauthier Folzan <gauthier@wuhu.eu>
2021-12-02 15:50:32 +0100

    Fichier donné en arguement

--- 01/julia_01a/julia_01a.jl
+++ 01/julia_01a/julia_01a.jl
@@ -1,4 +1,16 @@
-open("./input") do f
+using ArgParse
+function parse_commandline()
+   s = ArgParseSettings()
+   @add_arg_table s begin
+      "filename"
+      help = "input filename"
+      required = true
+   end
+   return parse_args(s)
+end
+function main()
+   parsed_args = parse_commandline()
+   open(parsed_args["filename"]) do f
    n_inc= 0
    prec_val= 9999
    for (i, line) in enumerate(eachline(f))
@@ -10,3 +22,5 @@ open("./input") do f
    end
    print("Total : $n_inc\n")
 end
+end
+main()
--- 01/julia_01b/julia_01b.jl
+++ 01/julia_01b/julia_01b.jl
@@ -1,16 +1,30 @@
-open("./input") do f
-   n_inc= 0
-   prec_val0= 9999
-   prec_val1= 9999
-   prec_val2= 9999
-   for (i, line) in enumerate(eachline(f))
-      new_val = parse(Int32, line)
-      if new_val > prec_val2
-          n_inc += 1;
-      end
-      prec_val2 = prec_val1;
-      prec_val1 = prec_val0;
-      prec_val0 = new_val;
+using ArgParse
+function parse_commandline()
+   s = ArgParseSettings()
+   @add_arg_table s begin
+      "filename"
+      help = "input filename"
+      required = true
    end
-   print("Total : $n_inc\n")
+   return parse_args(s)
 end
+function main()
+   parsed_args = parse_commandline()
+   open(parsed_args["filename"]) do f
+      n_inc= 0
+      prec_val0= 9999
+      prec_val1= 9999
+      prec_val2= 9999
+      for (i, line) in enumerate(eachline(f))
+         new_val = parse(Int32, line)
+         if new_val > prec_val2
+            n_inc += 1;
+         end
+         prec_val2 = prec_val1;
+         prec_val1 = prec_val0;
+         prec_val0 = new_val;
+      end
+      print("Total : $n_inc\n")
+   end
+end
+main()
